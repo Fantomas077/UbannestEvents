aa3ee20694e539b08be96dd2f9c290100054b6d5
Fantomas077 <nheltonn@gmail.com>
2025-04-29 00:08:39 +0200

    AddConnectionString To projet

--- UbannestEvents/Program.cs
+++ UbannestEvents/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using UbannestEvents.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
